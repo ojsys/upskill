ef709836b39fad2d56b4768a923c59d873943a23
Onah Jonah <onahjonah@gmail.com>
2024-10-28 21:12:12 +0100

    update on dash

--- accounts/views.py
+++ accounts/views.py
@@ -16,7 +16,7 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, f'Welcome back, {username}!')
-                return redirect('home')
+                return redirect('user_dashboard')
             else:
                 messages.error(request, "Invalid username or password.")
         else:
